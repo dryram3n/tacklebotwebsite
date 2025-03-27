f9db099736ea456b0b0e8fc157247967ab8e026b
dryram3n <garrett12082@gmail.com>
2025-03-27 12:46:26 -0400

    Fix fish image filenames to use spaces instead of underscores for consistency

--- fishImages.js
+++ fishImages.js
@@ -2,11 +2,11 @@
 const FISH_IMAGES = {
     // Common
     "Common Carp": "fishImages/Carp.png",
-    "Dark Carp": "fishImages/Dark_Carp.png",
+    "Dark Carp": "fishImages/Dark Carp.png",
     "Minnow": "fishImages/Minnow.png",
-    "Dark Minnow": "fishImages/Dark_Minnow.png",
+    "Dark Minnow": "fishImages/Dark Minnow.png",
     "Sardine": "fishImages/Sardine.png",
-    "Hungry Sardine": "fishImages/hungry_sardine.png", // Note: lowercase 'h' in filename
+    "Hungry Sardine": "fishImages/hungry sardine.png", // Note: lowercase 'h' in filename
     "Sunfish": "fishImages/Sunfish.png",
     "Bluegill": "fishImages/Bluegill.png",
     "Perch": "fishImages/Perch.png",
@@ -16,15 +16,15 @@ const FISH_IMAGES = {
 
     // Uncommon
     "Bass": "fishImages/Bass.png",
-    "Rainbow Trout": "fishImages/Rainbow_Trout.png",
+    "Rainbow Trout": "fishImages/Rainbow Trout.png",
     "Catfish": "fishImages/Catfish.png",
     "Mackeral": "fishImages/Mackeral.png", // Often spelled Mackerel
     "Pike": "fishImages/Pike.png",
     "Snapper": "fishImages/Snapper.png",
     "Crappie": "fishImages/Crappie.png",
-    "Luminous Eel": "fishImages/Luminous_Eel.png",
-    "Alluring Mermaid": "fishImages/Alluring_Mermaid.png",
-    "Transparant Shark": "fishImages/Transparent_Shark.png", // Corrected spelling in filename
+    "Luminous Eel": "fishImages/Luminous Eel.png",
+    "Alluring Mermaid": "fishImages/Alluring Mermaid.png",
+    "Transparant Shark": "fishImages/Transparent Shark.png", // Corrected spelling in filename
 
     // Rare
     "Salmon": "fishImages/Salmon.png",
@@ -34,54 +34,54 @@ const FISH_IMAGES = {
     "Piranha": "fishImages/Piranha.png",
     "Pufferfish": "fishImages/Pufferfish.png",
     "Mahi Mahi": "fishImages/Mahi-Mahi.png", // Note: hyphen
-    "Giant Sturgeon": "fishImages/Giant_Sturgeon.png",
-    "Electric Ray": "fishImages/Electric_Ray.png",
-    "Crystal Carp": "fishImages/Crystal_Carp.png",
+    "Giant Sturgeon": "fishImages/Giant Sturgeon.png",
+    "Electric Ray": "fishImages/Electric Ray.png",
+    "Crystal Carp": "fishImages/Crystal Carp.png",
     "Crawfish": "fishImages/Crawfish.png",
     "Snail": "fishImages/Snail.png",
 
     // Legendary
-    "Golden Koi": "fishImages/Golden_Koi_Fish.png", // Note: "Fish" added in filename
+    "Golden Koi": "fishImages/Golden Koi Fish.png", // Note: "Fish" added in filename
     "Gar": "fishImages/Gar.png",
-    "Giant Catfish": "fishImages/Giant_Catfish.png",
-    "Fish O Legend": "fishImages/Fish_O_Legend.png", // Note: hyphens
-    "Ancient Dragon Fish": "fishImages/Ancient_Dragon_Fish.png",
+    "Giant Catfish": "fishImages/Giant Catfish.png",
+    "Fish O Legend": "fishImages/Fish-O-Legend.png", // Note: hyphens
+    "Ancient Dragon Fish": "fishImages/Ancient Dragon Fish.png",
     "Anglerfish": "fishImages/Anglerfish.png",
-    "Ghostly Eel": "fishImages/Ghostly_Eel.png",
-    "Diamond Bass": "fishImages/Diamond_Bass.png",
-    "Prismatic Shark": "fishImages/Prismatic_Shark.png",
-    "Obsidian Marlin": "fishImages/Obsidian_Marlin.png",
-    "Gilded Coelacanth": "fishImages/Gilded_Coelacanth.png",
-    "Ethereal Seahorse": "fishImages/Ethereal_Seahorse.png",
+    "Ghostly Eel": "fishImages/Ghostly Eel.png",
+    "Diamond Bass": "fishImages/Diamond Bass.png",
+    "Prismatic Shark": "fishImages/Prismatic Shark.png",
+    "Obsidian Marlin": "fishImages/Obsidian Marlin.png",
+    "Gilded Coelacanth": "fishImages/Gilded Coelacanth.png",
+    "Ethereal Seahorse": "fishImages/Ethereal Seahorse.png",
 
     // Mythic
     "Kraken": "fishImages/Kraken.png",
-    "Rainbow Shark": "fishImages/Rainbow_Shark.png",
-    "Celestial Whale": "fishImages/Celestial_Whale.png",
-    "Starlight Jellyfish": "fishImages/Starlight_Jellyfish.png",
-    "Phoenix Fish": "fishImages/Phoenix_Fish.png",
-    "Galactic Ray": "fishImages/Galactic_Ray.png",
-    "Void Leviathan": "fishImages/Void_Leviathan.png",
-    "Benihana HotFish": "fishImages/Benihana_HotFish.png",
-    "NO NAME": "fishImages/NO_NAME.png",
-    "Aesthetic Spartan on a Fish": "fishImages/Aesthetic_Spartan_on_a_Fish.png",
+    "Rainbow Shark": "fishImages/Rainbow Shark.png",
+    "Celestial Whale": "fishImages/Celestial Whale.png",
+    "Starlight Jellyfish": "fishImages/Starlight Jellyfish.png",
+    "Phoenix Fish": "fishImages/Phoenix Fish.png",
+    "Galactic Ray": "fishImages/Galactic Ray.png",
+    "Void Leviathan": "fishImages/Void Leviathan.png",
+    "Benihana HotFish": "fishImages/Benihana HotFish.png",
+    "NO NAME": "fishImages/NO NAME.png",
+    "Aesthetic Spartan on a Fish": "fishImages/Aesthetic Spartan on a Fish.png",
     "Goldie": "fishImages/Goldie.png",
     "Ribbity": "fishImages/Ribbity.png",
-    "Stained Glass Fish": "fishImages/Stained_Glass_Fish.png",
+    "Stained Glass Fish": "fishImages/Stained Glass Fish.png",
 
     // Chimerical
-    "Dimensional Leviathan": "fishImages/Dimensional_Leviathan.png",
-    "Paradox Whale": "fishImages/Paradox_Whale.png",
-    "Quantum Kraken": "fishImages/Quantum_Kraken.png",
-    "Reality Ripper": "fishImages/Reality_Ripper.png",
-    "Time Serpent": "fishImages/Time_Serpent.png",
-    "Cosmic Angler": "fishImages/Cosmic_Angler.png",
-    "Void Maw": "fishImages/Void_Maw.png",
-    "Star Devourer": "fishImages/Star_Devourer.png",
+    "Dimensional Leviathan": "fishImages/Dimensional Leviathan.png",
+    "Paradox Whale": "fishImages/Paradox Whale.png",
+    "Quantum Kraken": "fishImages/Quantum Kraken.png",
+    "Reality Ripper": "fishImages/Reality Ripper.png",
+    "Time Serpent": "fishImages/Time Serpent.png",
+    "Cosmic Angler": "fishImages/Cosmic Angler.png",
+    "Void Maw": "fishImages/Void Maw.png",
+    "Star Devourer": "fishImages/Star Devourer.png",
     "Goatfish": "fishImages/Goatfish.png",
     "Goldie X": "fishImages/Goldie X.png",
     "Frogo": "fishImages/Frogo.png",
-    "Toxic Sludge Fish": "fishImages/Toxic_Sludge_Fish.png",
+    "Toxic Sludge Fish": "fishImages/Toxic Sludge Fish.png",
 
     "placeholder": "fishImages/Anchovy.png"
 };
